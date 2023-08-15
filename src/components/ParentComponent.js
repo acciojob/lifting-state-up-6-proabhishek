b836d66a9bd37b42f8539dfdc56d5d8d099f708c
Abhishek Shankar Choudhary <28715657+proabhishek@users.noreply.github.com>
2023-08-15 12:51:22 +0000

    test

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import TodoList from './TodoList';
+
+const ParentComponent = () => {
+  // Initial state of todos
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Learn React', completed: false },
+    { id: 2, text: 'Build a React App', completed: false },
+    { id: 2, text: 'Deploy the React App', completed: false },
+   
+  ]);
+
+  // Function to mark a todo as completed
+  const handleComplete = (id) => {
+    const updatedTodos = todos.map((todo) => 
+      todo.id === id ? { ...todo, completed: true } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <TodoList todos={todos} onComplete={handleComplete} />
+    </div>
+  );
+};
+
+export default ParentComponent;
---
+++ src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TodoList = ({ todos, onComplete }) => {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+          {todo.text}
+          <button onClick={() => onComplete(todo.id)}>Complete</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
